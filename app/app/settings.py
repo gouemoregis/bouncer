3ad737750c6f52250f8e81cf5a1ada9862836806
gouemoregis <gouemoregis@gmail.com>
2023-04-17 15:59:43 +0200

    Added datastore

--- app/app/settings.py
+++ app/app/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'app.datastore.NDBMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -126,3 +127,9 @@ LOGGING = {
         'level': 'DEBUG'
     }
 }
+
+IS_GAE = os.environ.get('GAE_APPLICATION', False)
+GOOGLE_CLOUD_PROJECT = os.environ.get('GOOGLE_CLOUD_PROJECT')
+DATASTORE_NAMESPACE = 'main'
+
+TEST_RUNNER = 'app.datastore.TestRunner'
